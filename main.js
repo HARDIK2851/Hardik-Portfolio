69b881fbdb944e2f4fb5d549a042336bd001b53d
Hardik Thakor <thakorhardik2851@gmail.com>
2025-02-19 22:56:06 -0500

    added comments in main.js file

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("#nav-links a");
     const sections = document.querySelectorAll("section");
 
+    // here active class is used to show section which is initially hidded
+
     function checkSections() {
         sections.forEach(section => {
             const sectionTop = section.getBoundingClientRect().top;
@@ -21,8 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // this function adds and removes active class from sections when nav links is clicked
-    // when any nav links is clicked, action class is added to section referenced by that link
-    // and action class is removed from other sections
+    // when any nav links is clicked, active class is added to section referenced by that link
+    // and active class is removed from other sections
     navLinks.forEach((link) => {
         link.addEventListener("click", function (event) {
             event.preventDefault(); 
@@ -32,13 +34,13 @@ document.addEventListener("DOMContentLoaded", function () {
             // this get section referenced by clicked link
             let targetSection = document.getElementById(targetId);
 
-            
+            // this removes active class from every other sections
             sections.forEach(section => section.classList.remove("active"));
 
-            
+            // this adds active class to targeted section
             targetSection.classList.add("active");
 
-            
+            // this allow smooth scrolling
             targetSection.scrollIntoView({ behavior: "smooth" });
         });
     });
