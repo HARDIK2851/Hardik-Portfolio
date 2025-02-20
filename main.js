b6395e550d193392b932ad1ec56f6efc454fa7e3
Hardik Thakor <thakorhardik2851@gmail.com>
2025-02-19 22:53:32 -0500

    added comments in main.js file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // this gets reference to naviagtion links and sections
     const navLinks = document.querySelectorAll("#nav-links a");
     const sections = document.querySelectorAll("section");
 
@@ -8,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
             const sectionBottom = section.getBoundingClientRect().bottom;
             const windowHeight = window.innerHeight;
 
+            // this checks if the section is comes in on screen active class is added to it
+            // if not active classes is removed from section
             if (sectionTop < windowHeight * 0.75 && sectionBottom > 0) {
                 section.classList.add("active");
             } 
@@ -17,11 +20,16 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    // this function adds and removes active class from sections when nav links is clicked
+    // when any nav links is clicked, action class is added to section referenced by that link
+    // and action class is removed from other sections
     navLinks.forEach((link) => {
         link.addEventListener("click", function (event) {
             event.preventDefault(); 
 
+            // this gets id of the link which is clicked
             let targetId = this.getAttribute("href").substring(1);
+            // this get section referenced by clicked link
             let targetSection = document.getElementById(targetId);
 
             
@@ -35,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    // this calls checkSection function when user scrolls through website
     window.addEventListener("scroll", checkSections);
     checkSections();
 });
